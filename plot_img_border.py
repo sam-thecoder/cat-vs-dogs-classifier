349c712cddc4141d6cf3af7c2268d065f2172c0c
Samuel Muiruri <muiruri.samuel@gmail.com>
2019-04-11 15:14:25 +0300

    step

--- plot_img_border.py
+++ plot_img_border.py
@@ -8,8 +8,12 @@ img = images[0]
 img = cv2.imread(img)
 
 edges = cv2.Canny(img, 250, 250)
+image_data = cv2.resize(edges, (64, 64))
 
 cv2.imshow('edges', edges)
+cv2.imshow('img', img)
+cv2.imshow('small icon', image_data)
+#cv2.imwrite('img.png', image_data)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
